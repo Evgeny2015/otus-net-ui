2ba44e60188c0f8ecc2caaa38fa878b65a06618d
evgeny3000 <AlekseevE2015@yandex.ru>
2025-05-08 18:12:34 +0300

    add pages, routes, hoc

---
+++ src/pages/login-page/login-page.tsx
@@ -0,0 +1,15 @@
+import { type FC } from 'react';
+// import './login-page.css';
+
+/*
+    Страница аутентификации
+*/
+const LoginPage: FC = () => {
+    return (
+        <>
+            <h2>Страница аутентификации</h2>
+        </>
+    )
+}
+
+export default LoginPage;
